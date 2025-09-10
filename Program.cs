7a31251b3deaade7478765061106bf70160a4b77
LuisVasquez13 <luis.vasquez14@utp.ac.pa>
2025-09-09 21:00:02 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//Programa para operadores de comparación
+
+int valor1, valor2;
+Console.WriteLine("Ingrese dos valores para compararlos:");
+valor1 = Convert.ToInt32(Console.ReadLine());
+valor2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Valores: {valor1}, {valor2}");
+
+bool resultado1 = valor1 == valor2;
+Console.WriteLine("El resultado de valor1 == valor2 es: " + resultado1);
+
+bool resultado2 = valor1 != valor2;
+Console.WriteLine("El resultado de valor1 != valor2 es: " + resultado2);
+
+bool resultado3 = valor1 > valor2;
+Console.WriteLine("El resultado de valor1 > valor2 es: " + resultado3);
+
+bool resultado4 = valor1 < valor2;
+Console.WriteLine("El resultado de valor1 < valor2 es: " + resultado4);
+
+bool resultado5 = valor1 >= valor2;
+Console.WriteLine("El resultado de valor1 >= valor2 es: " + resultado5);
+
+bool resultado6 = valor1 <= valor2;
+Console.WriteLine("El resultado de valor1 <= valor2 es: " + resultado6);
